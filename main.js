0206fcbfe9cf4883fca5d3ed97dc7ac9611b58f7
Sap Point <emptied1nside@gmail.com>
2024-01-29 18:00:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+// const map = new Map();
+// map.set("jojo",1)
+// map.set("bleach",2)
+// map.set("berserk",3)
+// console.log(map);
+
+// console.log(map.get("bleach"));
+// console.log(map.has("berserk"));
+// console.log(map.delete("jojo"));
+// console.log(map.clear());
+
+
+// const map = new Map();
+// map.set("jojo","1")
+// map.set("bleach","2a")
+// map.set("berserk", "a3")
+// console.log(map.values());
+
+// const arr = [...map.values()]
+
+// console.log(arr);
+
+// const result = arr.map(el => parseInt(el, 10))
+// console.log(result);
+
+
+// const mySet2 = new Set([1, 2, 7, 4,5]);
+// console.log(mySet2.size); // 4
+// console.log([...mySet2]); // [1, 2, 3, 4]
+
+// const set = new Set();
+// set.add("button").add("active").add("active")
+// console.log(set);
